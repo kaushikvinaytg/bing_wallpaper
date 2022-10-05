9ea03e54d404856554f56462c168d0430bb8e740
Kaushik Vinay T G <kaushik@jobadx.com>
2022-10-05 13:36:08 -0400

    Change how the current source image is looked up

--- changewallpaper.sh
+++ changewallpaper.sh
@@ -8,9 +8,9 @@ dir = "#{ENV['HOME']}/.bing_wallpaper"
 
 FileUtils.mkdir_p(dir)
 
-file_name = Dir.entries(dir).select { |f| File.file?("#{dir}/#{f}") }.first
+file_path = `osascript -e 'tell app "finder" to get posix path of (get desktop picture as alias)'`
 
-file_path = "#{dir}/#{file_name}" if file_name
+file_name = File.basename(file_path)&.strip
 
 URL = URI("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-CA&uhd=1")
 
@@ -21,9 +21,8 @@ image_url = json_body["images"][0]["url"]
 image_url.gsub!(/(1920|1080)/, {"1920" => "3024", "1080" => "1964"})
 
 new_file_name = image_url.match(/\?id=([a-zA-Z0-9._-]+)&/)[1]
-new_file_path = "#{dir}/#{new_file_name}"
 
-file_path_to_set = file_path
+new_file_path = "#{dir}/#{new_file_name}"
 
 if file_name != new_file_name
     Net::HTTP.start("www.bing.com") do |http|
@@ -32,8 +31,6 @@ if file_name != new_file_name
             file.write(resp.body)
         end
     end
-    file_path_to_set = new_file_path
-    File.delete(file_path) if file_name
+    `osascript -e 'tell application "Finder" to set desktop picture to POSIX file "#{new_file_path}"'`
 end
 
-`osascript -e 'tell application "Finder" to set desktop picture to POSIX file "#{file_path_to_set}"'`
