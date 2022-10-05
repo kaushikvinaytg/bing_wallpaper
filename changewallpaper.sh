bbcd905d88bb5be7fabc4c5dd601a75cf591e8a0
Kaushik Vinay T G <kaushik@jobadx.com>
2022-10-05 13:03:15 -0400

    tweaks to prevent unnecessary download of existing image

--- changewallpaper.sh
+++ changewallpaper.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
-require 'json'
-require 'fileutils'
-require 'digest'
+require "net/http"
+require "json"
+require "fileutils"
+require "digest"
+require "securerandom"
 
-FileUtils.mkdir_p("#{ENV['HOME']}/.bing_wallpaper")
+dir = "#{ENV['HOME']}/.bing_wallpaper"
 
-file_path = "#{ENV['HOME']}/.bing_wallpaper/wallpaper.jpg"
+FileUtils.mkdir_p(dir)
 
-img_hash = Digest::MD5.hexdigest(File.read(file_path)) if File.exists?(file_path)
+file_name = Dir.entries(dir).select { |f| File.file?("#{dir}/#{f}") }.first
+
+file_path = "#{dir}/#{file_name}" if file_name
 
 URL = URI("https://www.bing.com/HPImageArchive.aspx?format=js&idx=20&n=1&mkt=en-CA&uhd=1")
 
@@ -17,17 +20,22 @@ response = Net::HTTP.get(URL)
 json_body = JSON.parse(response)
 
 image_url = json_body["images"][0]["url"]
-image_url.gsub!(/(1920|1080)/, {'1920' => '3024', '1080' => '1964'})
+image_url.gsub!(/(1920|1080)/, {"1920" => "3024", "1080" => "1964"})
 
-Net::HTTP.start("www.bing.com") do |http|
-    resp = http.get(image_url)
-    open(file_path, "w+") do |file|
-        file.write(resp.body)
+new_file_name = image_url.match(/\?id=([a-zA-Z0-9._-]+)&/)[1]
+new_file_path = "#{dir}/#{new_file_name}"
+
+file_path_to_set = file_path
+
+if file_name != new_file_name
+    Net::HTTP.start("www.bing.com") do |http|
+        resp = http.get(image_url)
+        open(new_file_path, "w+") do |file|
+            file.write(resp.body)
+        end
     end
+    file_path_to_set = new_file_path
+    File.delete(file_path) if file_name
 end
 
-img_hash_new = Digest::MD5.hexdigest(File.read(file_path))
-
-if img_hash_new != img_hash
-    `osascript -e 'tell application "Finder" to set desktop picture to POSIX file "#{file_path}"'`
-end
+`osascript -e 'tell application "Finder" to set desktop picture to POSIX file "#{file_path_to_set}"'`
